0a3625eb37f9c06359d71fd8b8bc64ce0d93b1d9
Adu Bright Kwarteng snr <adubrightkwarrteng11@gmail.com>
2023-01-23 08:50:31 +0000

    created and modified tweet form creation

--- chattapp/tweets/urls.py
+++ chattapp/tweets/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
      path('',views.home,name="home"),
      path('<int:tweet_id>',views.detail_page,name='detail'),
-     path('tweet_view',views.tweets_view,name='tweet_view')
+     path('tweet_view',views.tweets_view,name='tweet_view'),
+     path('create_tweet',views.tweet_create_view, name = 'create_tweet')
      
 ]
 
--- chattapp/tweets/views.py
+++ chattapp/tweets/views.py
@@ -1,43 +1,56 @@
 import random
 from django.shortcuts import render
 from django.template import loader
-from django.http import Http404, HttpResponse,JsonResponse
-
+from django.http import Http404, HttpResponse, JsonResponse
 from .models import Tweet
+from .forms import TweetForm
+
 
 # Create your views here.
 
-def home(request,*args,**kwargs):
+def home(request, *args, **kwargs):
     context = {
-        
-    }
-    return render(request,"tweets/home.html",context = context,status=200)
 
-def tweets_view(request,*args,**kwargs):
+    }
+    return render(request, "tweets/home.html", context=context, status=200)
+
+
+def tweets_view(request, *args, **kwargs):
     """"
     REST API TO BE CONSUMED BY THE FRONTEND
     """
     query_set = Tweet.objects.all()
-    tweet_list = [{'id':tweet.id, 'content':tweet.content,'likes':random.randint(0,100) } for tweet in query_set]
+    tweet_list = [{'id': tweet.id, 'content': tweet.content, 'likes': random.randint(0, 100)} for tweet in query_set]
     data = {
-        'isUser':False,
-        'response':tweet_list
+        'isUser': False,
+        'response': tweet_list
     }
     return JsonResponse(data)
 
-def detail_page(request,tweet_id,*args,**kwargs):
-# REST API VIEW FOR THE CONTENT
+
+def detail_page(request, tweet_id, *args, **kwargs):
+
+    #REST  API VIEW FOR THE CONTENT
+    # Return json data
+    # Consume by Javascript
     data = {
-            "id" : tweet_id
+        "id": tweet_id
     }
     status = 200
     try:
-        obj= Tweet.objects.get(id = tweet_id)
+        obj = Tweet.objects.get(id=tweet_id)
         data["content"] = obj.content
         # data['image'] = obj.image.url
     except:
         data["message"] = "Not Found"
         status = 404
 
-    
-    return JsonResponse(data,status=status)
+    return JsonResponse(data, status=status)
+
+def tweet_create_view(request,*args,**kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj =form.save(commit=False)
+        obj.save()
+        form = TweetForm()
+    return render(request,'components/form.html',context = {'form':form})
